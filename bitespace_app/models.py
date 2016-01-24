c4e8b23e93d07dad41e1261a1d144f74804650c7
Srijan Narang <shubham@jeevomics.com>
2016-01-24 15:44:42 +0530

    removed unwanted files

--- bitespace_app/models.py
+++ bitespace_app/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class USDAIngredient(models.Model):
     food_id = models.CharField(primary_key=True, max_length=10)
     name = models.TextField()
-    water = models.DecimalField(max_digits=20,
-                                decimal_places=10, null=True)
+    ##water = models.DecimalField(max_digits=20,
+                                ##decimal_places=10, null=True)
     def __unicode__(self):
     	return self.name
