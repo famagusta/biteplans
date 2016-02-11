43fdfad5274f5a074981a1bf965da03f1a1a56a1
Srijan Narang <shubham@jeevomics.com>
2016-02-12 00:46:23 +0530

    Full text search implemented, see readme to setup sql to do it

--- bitespace_app/views.py
+++ bitespace_app/views.py
@@ -1,5 +1,4 @@
 '''api views for our bitespace_app'''
-from rest_framework import generics
 from bitespace_app.models import USDAIngredient
 from bitespace_app.serializers import GlobalSearchSerializer
 from rest_framework.views import APIView
@@ -7,15 +6,17 @@ from rest_framework.response import Response
 
 class GlobalSearchList(APIView):
     '''creates serializer of the queryset'''
-    def get(self,request):
-        query = self.request.query_params.get('query', None)
-        result = USDAIngredient.objects.filter(shrt_desc__icontains=query)
-        result = GlobalSearchSerializer(result,many=True)
+    def get(self, request):
+        '''Handles get request'''
+        query = request.query_params.get('query', None)
+        result = USDAIngredient.objects.filter(shrt_desc__search=query)
+        result = GlobalSearchSerializer(result, many=True)
         return Response(result.data)
 
-    def post(self,request):
+    def post(self, request):
+        '''Handles post request'''
         print self.request.POST
-        query = self.request.POST.get('query',False)
-        result = USDAIngredient.objects.filter(shrt_desc__icontains=query)
-        result = GlobalSearchSerializer(result,many=True)
+        query = request.POST.get('query', False)
+        result = USDAIngredient.objects.filter(shrt_desc__search=query)
+        result = GlobalSearchSerializer(result, many=True)
         return Response(result.data)
