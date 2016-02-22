78bc90f36c625447fc3de28cc3fe84ad829cf23c
shubh3794 <shubham@jeevomics.com>
2016-02-23 02:05:17 +0530

    fixed the modal view KINDLY READ THE COMMENTS AND PREFER NOT TO TOUCH

--- static/javascripts/controllers/navbarController.js
+++ static/javascripts/controllers/navbarController.js
@@ -5,32 +5,40 @@ app.controller('navbarController', ['$scope', '$location', function ($scope, $lo
         var currentRoute = $location.path().substring(1) || '/';
         return page === currentRoute ? 'active' : '';
     };
-    
+    //by default first modal viw will be visible;
     $scope.modal1 = true;  
     $scope.modal2 = false;
     $scope.modal3 = false;        
 
+//Function to switch the views within modal;
     $scope.switchToModal = function (number) {     // function to change the content in modal window
    
     console.log(number+typeof(number));
+
+    //show second modal view
     
-       if (number == 2) {
+       if (number===2) {
            $scope.modal2 = true;
            $scope.modal1 = false;
            $scope.modal3 = false;
         }
-       
-        else if (number == 3) {
+     
+    //show third modal view  
+        else if (number===3) {
            $scope.modal3 = true;
            $scope.modal1 = false;
            $scope.modal2 = false;
         }
-        
+    //show default that is first modal view    
         else {
            $scope.modal1 = true;
            $scope.modal2 = false;
            $scope.modal3 = false;
         }
-    }
+    };
+//Function to open the modal on clicking login button in the nav
+    $scope.openModal = function(){
+      $('#modal1').openModal();
+    };
     
 }]);
