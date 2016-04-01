1dae22cf586c30002496022d7e83a1c6b27bfb41
Jeevomics <keshav@jeevomics.com>
2016-04-01 15:46:05 +0530

    Create plans updated

--- static/javascripts/controllers/createPlanController.js
+++ static/javascripts/controllers/createPlanController.js
@@ -1,7 +1,10 @@
+// controller for create plans page
 'use strict';
 
 app.controller('createPlanController', ['$scope', 'ingredientService', function($scope, ingredientService) {
+  
    
+    // function to search ingredients in create plan modal
     $scope.searchPlan = function(query) {
      if (query) {
            ingredientService.search(query).then(function(response) {
@@ -13,6 +16,8 @@ app.controller('createPlanController', ['$scope', 'ingredientService', function(
         }
     }
          
+    
+    // switch views among differnt steps in creating  plans
     $scope.createPlanView1 = true;
     $scope.createPlanView2 = false;
     $scope.createPlanView3 = false;
@@ -62,14 +67,17 @@ app.controller('createPlanController', ['$scope', 'ingredientService', function(
         
     }
     
-    $scope.nutrientValue = [];
+    $scope.nutrientValue = []; // array to store the nutrient values in modal for every ingredient checked
     
-    $scope.nutrientDisplay = [];
+    // separate array for each meal
+    $scope.breakfastDisplay = [];
+    $scope.lunchDisplay = [];
+    $scope.dinnerDisplay = [];
+    $scope.snacksDisplay = [];
     
-    console.log($scope.nutrientDisplay);
-    
-    $scope.emptyModalContents = function () {
-        if ($scope.mealName == 'Breakfast') {
+    // copies the ingredients selected in the respective meal array
+    $scope.addContents = function () {
+         if ($scope.mealName == 'Breakfast') {
             $scope.breakfastDisplay = $scope.nutrientValue.slice(0);
         }
          if ($scope.mealName == 'Lunch') {
@@ -82,20 +90,77 @@ app.controller('createPlanController', ['$scope', 'ingredientService', function(
             $scope.snacksDisplay = $scope.nutrientValue.slice(0);
         }
         
-        $scope.nutrientValue.length = 0;
+        $('#create-plan-modal').closeModal();
+    }
+   
+    // uncheck all the selected items if save button is not clicked
+    $scope.emptyModalContents = function () {
+       
+        $scope.breakfastDisplay.length = 0;
+        $scope.lunchDisplay.length = 0;
+        $scope.dinnerDisplay.length = 0;
+        $scope.snacksDisplay.length = 0;
         
         $('#create-plan-modal').closeModal();
 
     }
     
-    $scope.saveAndContinue = function () {
-       
+    
+    $scope.amount = 1; // serving per ingredient
+
+    // to fire red(-) button which removes the entire meal
+    $scope.clearMeal = function () {
+       if ($scope.mealName == 'Breakfast') {
+            $scope.breakfastDisplay.length = 0;
+        }
+         if ($scope.mealName == 'Lunch') {
+            $scope.lunchDisplay.length = 0;
+        }
+         if ($scope.mealName == 'Dinner') {
+            $scope.dinnerDisplay.length = 0;
+        }
+         if ($scope.mealName == 'Snacks') {
+            $scope.snacksDisplay.length = 0;
+        }
     }
     
-    // function to delete ingredients
+    // function to delete ingredients from modal
     $scope.removeIngredient = function (element) {
         var index = $scope.nutrientValue.indexOf(element);
         $scope.nutrientValue.splice(index, 1);     
     }
+    
+    // function to delete meal from the saved ingredients
+    $scope.removeSavedIngredient = function (num) {
+        if (num == 1) {
+           var index = $scope.breakfastDisplay.indexOf(num);
+           $scope.breakfastDisplay.splice(index, 1); 
+        }
+        if (num == 2) {
+           var index = $scope.lunchDisplay.indexOf(num);
+           $scope.lunchDisplay.splice(index, 1); 
+        }
+        if (num == 3) {
+           var index = $scope.dinnerDisplay.indexOf(num);
+           $scope.dinnerDisplay.splice(index, 1); 
+        }
+        if (num == 4) {
+           var index = $scope.snacksDisplay.indexOf(num);
+           $scope.snacksDisplay.splice(index, 1); 
+        }
+        
+    }
+    
+    $scope.dayCount = 1 
+    
+    $scope.weekCount = 1;
+    
+    $scope.addMealName = {};
+    
+    $scope.addMealName.meal1 = 'Breakfast';
+    $scope.addMealName.meal2 = 'Lunch';
+    $scope.addMealName.meal3 = 'Dinner';
+    $scope.addMealName.meal4 = 'Snacks';
+    
     
 }]);
