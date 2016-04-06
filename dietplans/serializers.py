cff21a775d97e486c2c759646e9eaee5cf7ce2e8
shubh3794 <shubham@jeevomics.com>
2016-04-06 16:07:12 +0530

    Minor bug

--- dietplans/serializers.py
+++ dietplans/serializers.py
@@ -1,9 +1,9 @@
 '''Serializers convert the db queries into python
 data structures(dictionaries) for easy json rendering'''
 from rest_framework import serializers
-from dietplans.models import DietPlan, DayPlan, MealPlan, MealRecipe
+from dietplans.models import DietPlan, DayPlan, MealPlan, MealRecipe, MealIngredient
 from recipes.serializers import RecipeSerializer
-
+from ingredients.serializers import IngredientSerializer
 class DietPlanSerializer(serializers.ModelSerializer):
 	'''Serializer to convert the recieved data into suitable python dict'''
 	class Meta:
@@ -19,10 +19,11 @@ class MealRecipeSerializer(serializers.ModelSerializer):
 
 class MealIngredientSerializer(serializers.ModelSerializer):
 	'''Serializer to convert the recieved data into suitable python dict'''
-	reciple = RecipeSerializer(many=False, read_only=True)
+	ingredient = IngredientSerializer(many=False, read_only=True)
+	measure = IngredientSerializer(many=False, read_only=True)
 	class Meta:
 		'''Meta data, or config for the serializer'''
-		model = MealRecipe
+		model = MealIngredient
 
 class MealPlanSerializer(serializers.ModelSerializer):
 	'''Serializer to convert the recieved data into suitable python dict'''
